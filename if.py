d65ff57a1a43d130cb1b51056712f87d481a3ad8
DC <hkhaoh@naver.com>
2017-03-20 10:19:11 +0900

    g

---
+++ if.py
@@ -0,0 +1,10 @@
+ninja = 5
+
+if ninja < 10:
+    print("i can't fight those ninja")
+if ninja < 30:
+    print('it will be a struggle, but i can take em')
+if ninja < 50:
+    print("that's too many")
+          
+    
