84c49ceed6f1d5d51c50989eed02c6410ed718a7
rodrigolll <hkhaoh@naver.com>
2017-04-02 14:55:51 +0900

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+for i in range(0,11):
+    i = i * 2 
+for j in range(0,11):
+    k = i + j
+    if k != 10: continue
+    print('%s + %s = %s'  % (i,j,k))
+              
+        
+        
+        
+    
+    
+
+
+
