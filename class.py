4e302391e196c3587e58b79c05d40af39695f4c8
rodrigolll <hkhaoh@naver.com>
2017-04-09 14:06:50 +0900

    Add files via upload

---
+++ class.py
@@ -0,0 +1,19 @@
+class 기린():
+    def 왼발앞(self):
+        print('왼발앞으로')
+    def 오른발앞(self):
+        print('오른발앞으로')
+    def 왼발뒤(self):
+        print('왼발뒤로')
+    def 오른발뒤(self):
+        print('오른발뒤로')
+
+    def 춤추기(self):
+        self.왼발앞()
+        self.왼발뒤()
+        self.오른발앞()
+        self.오른발뒤()
+        self.왼발뒤()
+        self.오른발뒤()
+        self.왼발앞()
+        self.오른발앞()
